b1f7df0152f37336a0c7c8d2c9a2c1640cb8dab5
Tomas Zulgis <timoras007@gmail.com>
2017-03-03 21:30:34 +0200

    robot after review

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,39 +1,39 @@
 class Robot
-    @@used_name = []
+  @@used_name = []
 
-    def initialize
-        @name = rnd_robot_name
-    end
+  def initialize
+    @name = rnd_robot_name
+  end
     
-    def name
-        @name
-    end
+  def name
+    @name
+  end
 
-    def reset
-        @name = rnd_robot_name
-    end
+  def reset
+    @name = rnd_robot_name
+  end
 
 
-    private 
-    def rnd_char
-        (65 + rand(26)).chr
-    end
+  private 
+  def rnd_chars
+    ('A'.ord + rand(26)).chr << ('A'.ord + rand(26)).chr
+  end
 
-    def rnd_digit
-        rand(1000).to_s.rjust(3, "0")
-    end
+  def rnd_digit
+    ('0'.ord + rand(10)).chr << ('0'.ord + rand(10)).chr << ('0'.ord + rand(10)).chr
+    
+  end
 
-    def rnd_robot_name 
-        new_name = "" << rnd_char << rnd_char << rnd_digit
-        if @@used_name.include? (new_name)
-            new_name = rnd_robot_name
-        end        
-        @@used_name << new_name
-        new_name
-    end
+  def rnd_robot_name 
+    new_name = "" << rnd_chars << rnd_digit
+    if @@used_name.include? (new_name)
+      new_name = rnd_robot_name
+    end        
+    @@used_name << new_name
+    new_name
+  end
 end
 
-
 module BookKeeping
-    VERSION = 2x
+  VERSION = 2
 end
